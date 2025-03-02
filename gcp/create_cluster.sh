91f255318c61d1de614fb9e72226db617e8f0865
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-03-02 11:13:58 +0100

    Select lower boot disk size for the VMs.

--- gcp/create_cluster.sh
+++ gcp/create_cluster.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-PROJECT_ID=my-light-cluster  # Replace with your PROJECT_ID
-REGION=us-central1 # Replace with your preferred REGION
+PROJECT_ID=adcoar-452409  # Replace with your project ID
+REGION=us-central1 # Replace with your desired region
 ZONES=("$REGION-a" "$REGION-a" "$REGION-b" "$REGION-b" "$REGION-c" "$REGION-c" "$REGION-f" "$REGION-f")
 
 for i in {1..8}; do
   gcloud compute instances create light-cluster-$i \
-    --project=$PROJECT_ID \
-    --zone=${ZONES[$i-1]} \
+    --project="$PROJECT_ID" \
+    --zone="${ZONES[$i-1]}" \
     --machine-type=custom-2-4096 \
-    --image-family=centos-7 \
+    --image-family=centos-stream-9 \
     --image-project=centos-cloud \
-    --boot-disk-size=50GB \
+    --boot-disk-size=20GB \
     --boot-disk-type=pd-standard \
-    --tags=light-cluster \
-    --no-address  # No external IP, internal cluster (optional)
-  echo "Created light-cluster-$i in ${ZONES[$i-1]}"
+    --tags=light-cluster
+  if [ $? -eq 0 ]; then
+    echo "Successfully created light-cluster-$i in ${ZONES[$i-1]}"
+  else
+    echo "Failed to create light-cluster-$i in ${ZONES[$i-1]}"
+  fi
 done
