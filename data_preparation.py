f2a42f63724863cd9d3d04948ac7a655a41c5000
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-03-03 11:16:32 +0100

    Change dataset size creation.

--- data_preparation.py
+++ data_preparation.py
@@ -4,7 +4,7 @@
 import os
 import shutil
 
-def create_image_copies(image_path, output_dir, num_copies=50):
+def create_image_copies(image_path, output_dir, num_copies=1024):
     """Creates multiple copies of an image with sequential filenames."""
     
     # Ensure output directory exists.
