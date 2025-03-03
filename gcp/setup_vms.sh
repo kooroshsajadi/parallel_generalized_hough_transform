e0473171638744ae60c0bd9cae8c031e759e0d08
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-03-03 10:40:20 +0100

    Remove -y attribute for opencv and opencv-devel installation.

--- gcp/setup_vms.sh
+++ gcp/setup_vms.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-PROJECT_ID=adcoar-452409  # Replace with your project ID
-REGION=us-central1 # Replace with your desired region
-ZONES=("$REGION-a" "$REGION-a" "$REGION-b" "$REGION-b" "$REGION-c" "$REGION-c" "$REGION-f" "$REGION-f")
-
-for i in {1..8}; do
-  echo "Setting up light-cluster-$i in ${ZONES[$i-1]}"
-  gcloud compute ssh light-cluster-$i --zone="${ZONES[$i-1]}" --project="$PROJECT_ID" --command="
+VMS=("10.128.0.6" "10.128.0.7" "10.128.0.8" "10.128.0.9" "10.128.0.10" "10.128.0.11" "10.128.0.12")
+for vm in "${VMS[@]}"; do
+  echo "Setting up $vm"
+  ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $vm "
+    echo 'seyedkourosh_sajjadi01 ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/90-user &&
     sudo yum update -y &&
-    sudo yum install -y epel-release gcc gcc-c++ make centos-release-scl &&
-    sudo yum install -y openmpi openmpi-devel opencv opencv-devel zlib-devel &&
+    sudo yum install -y epel-release gcc gcc-c++ make openmpi openmpi-devel zlib-devel &&
+    yes | sudo yum install opencv opencv-devel &&
     echo 'module load mpi/openmpi-x86_64' >> ~/.bashrc
   "
   if [ $? -eq 0 ]; then
-    echo "Successfully set up light-cluster-$i"
+    echo "Successfully set up $vm"
   else
-    echo "Failed to set up light-cluster-$i"
+    echo "Failed to set up $vm"
   fi
 done
