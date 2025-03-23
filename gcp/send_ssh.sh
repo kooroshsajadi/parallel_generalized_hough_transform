853b3186ac29a415ab038a7bd4fc7c27c723fe79
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2025-03-23 14:51:15 +0100

    Remove redundant bash file.

--- gcp/send_ssh.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..8}; do
-  gcloud compute scp id_rsa.pub light-cluster-$i:~/Documents/infossh \
-    --zone=us-central1-$(printf "%c" $(($i % 4 + 97))) \
-    --project=adcoar-452409
-done
